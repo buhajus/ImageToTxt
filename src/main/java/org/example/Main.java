b9f7bbb3a9e6b23de25f9d475952f092176290cf
Buhajus <domassteimantas@gmail.com>
2023-04-02 22:50:39 +0300

    v.0.1.1

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -16,8 +16,12 @@ import java.text.SimpleDateFormat;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.ZoneId;
+import java.util.Arrays;
 import java.util.Date;
+import java.util.List;
+import java.util.stream.Collectors;
 
+import static org.opencv.imgcodecs.Imgcodecs.IMREAD_ANYCOLOR;
 import static org.opencv.imgcodecs.Imgcodecs.IMREAD_GRAYSCALE;
 
 public class Main {
@@ -28,43 +32,60 @@ public class Main {
 
         Tesseract tesseract = new Tesseract();
         Imgcodecs imgcodecs = new Imgcodecs();
-       // Date date = new Date();
+        // Date date = new Date();
         LocalDate localDate = LocalDate.now();
-       // LocalDateTime dateTime = LocalDateTime.now(ZoneId.of("Europe/Vilnius"));
-       // System.out.println(dateTime);
+        // LocalDateTime dateTime = LocalDateTime.now(ZoneId.of("Europe/Vilnius"));
+        // System.out.println(dateTime);
         //SimpleDateFormat formater = new SimpleDateFormat("YYYY-mm-dd");
 
-        String path = "src/img/4.jpg";
-        String savePath = "src/img/records_"+ localDate +".jpg";
+        String path = "src/img/3.jpg";
+        String savePath = "src/img/records_" + localDate + ".jpg";
         // tesseract.setDatapath("tessdata/*");
-        tesseract.setLanguage("lit");
-        // reading image
+        tesseract.setLanguage("eng");
+
+
 
         Mat img = loadImages(path);
-       HighGui.imshow("Image ", img);
-           Rect rectCrop = new Rect(190, 55, 150, 100);
-          Mat cropped = new Mat(img,rectCrop);
-        //  Mat src = new Mat();
-          Mat dst = new Mat();
-         Mat kernel = Mat.ones(5, 5, CvType.CV_32F);
+        System.out.println(img);
+        Mat newImg = cropImages(img);// crop loaded image
+
+        Mat dst = new Mat();
+        Mat kernel = Mat.ones(0, 200, CvType.CV_32F);
 
 
-        Imgproc.bilateralFilter(cropped, dst, 15, 80, 80, Core.BORDER_DEFAULT);
-          // Imgproc.morphologyEx(cropped, dst, Imgproc.MORPH_CLOSE, kernel);
+       Imgproc.bilateralFilter(newImg, dst, 5, 50, 50, Core.BORDER_DEFAULT);
+        // Imgproc.morphologyEx(newImg, dst, Imgproc.MORPH_OPEN, kernel);
 
 
         //write the image
-        HighGui.imshow("Img", cropped);
-        imgcodecs.imwrite(savePath,cropped);
+
+        imgcodecs.imwrite(savePath,dst);
+
+
 
         String result = tesseract.doOCR(new File(savePath));
+        List <String>  list = Arrays.asList(result);
         System.out.println(result);
+     //   System.out.println(result.toString());
+
+
+        HighGui.imshow("Img", dst);
+        HighGui.moveWindow("Img",1500,200);
+        HighGui.waitKey(0);
+
+
     }
 
     public static Mat loadImages(String path) {
         Imgcodecs imgcodecs = new Imgcodecs();
         return imgcodecs.imread(path, IMREAD_GRAYSCALE);
-        // return imgcodecs.imread(path );
+
+    }
+
+    public static Mat cropImages(Mat matrix) {
+        Rect rectCrop = new Rect(0, 145, 200, 50);
+        Mat cropped = new Mat(matrix, rectCrop);
+        return cropped;
     }
 
 
